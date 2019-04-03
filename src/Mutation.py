2dec39559b6c8fc8990ada7bb5504ddf8f7088ed
mforberg <mifor16@student.sdu.dk>
2019-04-03 09:47:22 +0200

    better mutation, no more print BabyRage

--- src/Mutation.py
+++ src/Mutation.py
@@ -6,28 +6,31 @@ def mutate_population(population):
 
     for solution in population:
         buildinglist = solution
-
         mutation_trigger = int(MUTATION_RATE * 100)
 
         for i in buildinglist:
-            randomnumber = random.randint(1, 100)
-            if randomnumber == mutation_trigger:
+            random_number_x = random.randint(1, 100)
+
+            if random_number_x <= mutation_trigger:
                 mutation_count += 1
 
-                x_or_z_decider = random.randint(1, 100)
                 add_or_subtract_decider = random.randint(1, 100)
 
-                if x_or_z_decider <= 50:
-                    if add_or_subtract_decider >= 50:
-                        i.x = i.x + 1
-                    else:
-                        i.x = i.x - 1
+                if add_or_subtract_decider > 50:
+                    i.x = i.x + 1
                 else:
-                    if add_or_subtract_decider >= 50:
-                        i.z = i.z + 1
-                    else:
-                        i.z = i.z - 1
-    number = float(mutation_count) / ((POPULATION_SIZE * GENE_SIZE)) * 100
+                    i.x = i.x - 1
+            random_number_z = random.randint(1, 100)
+
+            if random_number_z <= mutation_trigger:
+                mutation_count += 1
+
+                add_or_subtract_decider = random.randint(1, 100)
+                if add_or_subtract_decider >= 50:
+                    i.z = i.z + 1
+                else:
+                    i.z = i.z - 1
+
+    number = float(mutation_count) / ((POPULATION_SIZE * GENE_SIZE * 2)) * 100
     percent = round(number, 3)
-    print "MUTATION TRIGGERED {0}/{1} TIMES ({2}%)".format(mutation_count, POPULATION_SIZE * GENE_SIZE, percent)
-    return population
+    print "MUTATION TRIGGERED {0}/{1} TIMES ({2}%)".format(mutation_count, POPULATION_SIZE * GENE_SIZE * 2, percent)
