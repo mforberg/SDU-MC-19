59393ec087f51ee6a518b61eef7e31a78c3cb777
JorgeBaezGarrido <joole16@student.sdu.dk>
2019-04-08 08:22:10 +0200

    Name changes
    - Name changing of methods and variables

--- variables/GA_VALUES.py
+++ variables/GA_VALUES.py
@@ -1,10 +1,10 @@
 """The amount of houses in a single solution"""
-GENE_SIZE = 15
+GENE_SIZE = 5
 """The percent chance of crossover"""
 CROSSOVER_RATE = 0.05 # 5%
 """The percent chance of mutation"""
 MUTATION_RATE = 0.01 # 1%
 """The amount of "solutions" in a population"""
-POPULATION_SIZE = 500
+POPULATION_SIZE = 2
 """The amount of generations the algorithm should be run"""
 GENERATIONS = 500
